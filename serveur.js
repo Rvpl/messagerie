41eddf77403d1988d62fe5b6f4b8e986e36d4499
Raphael Mezui <raphael.mezui@icloud.com>
2022-03-25 18:14:55 +0100

    parametrage deploiement api

---
+++ serveur.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path =  require("path");
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/messagerie'));
+
+app.get('/*', function (req,res){
+  res.sendFile(path.join(__dirname + 'dist/messagerie/index.html'))
+})
+
+app.listen(process.env.PORT);
+
